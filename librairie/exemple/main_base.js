09222aa3c8110ec1e317c20dc22992f5c267bd1a
Damien Jakob <damien.jakob@cpnv.ch>
2019-11-29 09:46:51 +0100

    Add done work

---
+++ librairie/exemple/main_base.js
@@ -0,0 +1,124 @@
+//--------------------------------------------------------------------------------------------
+// Exercice GUI2 Vue.js
+// CPNV - AMR
+// Jan 2018
+//--------------------------------------------------------------------------------------------
+
+
+//--------------------------------------------------------------------------------------------
+// Wrapper pour gérer la gestion des événements entre composants
+
+window.Event=new class{
+    constructor(){
+        this.vue=new Vue();
+    }
+    fire(event,data = null){
+        this.vue.$emit(event, data);
+    }
+    listen(event, callback){
+        this.vue.$on(event, callback);
+    }
+};
+ 
+//--------------------------------------------------------------------------------------------
+// Mixin contenant les fonctions utilisées par plusieurs composants
+
+var myMixin = {
+    methods: {
+        icone:function(cat){            
+            switch(cat) {
+                case "moto":
+                    icone="fa fa-motorcycle my-darkblue";
+                    break;
+                case "van":
+                    icone="fa fa-bus my-red";
+                    break;
+                case "velo":
+                    icone="fa fa-bicycle my-green";
+                    break;
+                default:
+                    icone="fa fa-car my-orange";
+            }           
+            return icone;
+        }
+    }
+  }
+
+//--------------------------------------------------------------------------------------------
+// Composant listevehicules. Affiche les vehicules dans une liste 'customisée'.
+
+var listeVehicules = Vue.component('liste-vehicules', {
+    template: '#listeVehicules',   
+    data: function() {
+        return {   
+            items:[]                          
+        }
+    },
+    mounted: function () {
+        this.fetchData();                  
+    },     
+    methods: {      
+        fetchData: function () {
+        this.items = vehicules;      
+        },  
+        setVehicule: function (option) {            
+            Event.fire('changeVehicule',option);            
+        },
+        getRightIcon(category) {
+            return this.$parent.getRightIcon(category)
+        }
+    }
+    });
+
+//--------------------------------------------------------------------------------------------
+// Composant detailVehicule. Affiche les détails d'un vehicule avec une animation.
+
+var detailsVehicule = Vue.component('details-vehicule', {
+    template: '#detailsVehicule',
+    mixins: [myMixin],
+    data: function() {
+        return {     
+            selectedItem:[],
+                 
+        }
+    },
+    mounted: function () {
+        this.fetchData();   
+        Event.listen('changeVehicule',function(n){  
+            this.changeValeurs(n);             
+        }.bind(this)) ;
+    },
+    methods: {      
+        fetchData: function () {
+                                            
+        },     
+        changeValeurs:function(n){          
+               
+        },
+        getRightIcon(category) {
+            return this.$parent.getRightIcon(category)
+        }
+    }
+    });
+
+//--------------------------------------------------------------------------------------------
+// Application principale Vue
+
+var app = new Vue({       
+    el: '#app',
+    methods: {
+        getRightIcon(category) {
+            switch (category) {
+                case 'velo':
+                    return 'fa fa-bicycle my-green'
+                case 'moto':
+                    return 'fa fa-motorcycle my-darkblue'
+                case 'voiture':
+                    return 'fa fa-car my-orange'
+                case 'van':
+                    return 'fa fa-bus my-red'
+            }
+        }  
+    }
+    });
+    
---
+++ librairie/main.js
@@ -0,0 +1,142 @@
+// Rem : we don't use the vehicle ids, because we use their index
+
+// Mixins
+// Elements reused by multiple components
+let vehicleMixin = {
+    // props : data that must be specified by the parent container
+    // (here : <vehicleListItem v-bind:vehicle="...">)
+    props: ['vehicle'],
+    computed: {
+        /**
+         * Get the icon corresponding to a vehicle
+         * @returns {string} the class of the icon of the vehicle
+         */
+        icon() {
+            let icon = '';
+            // check the category of the vehicle
+            switch (this.vehicle.cat) {
+                case "moto":
+                    icon = 'fa fa-motorcycle my-darkblue';
+                    break;
+                case "velo":
+                    icon = 'fa fa-bicycle my-green';
+                    break;
+                case "van":
+                    icon = 'fa fa-bus my-red';
+                    break;
+                case "voiture":
+                // if category not recognized, display the car icon
+                default:
+                    icon = 'fa fa-car my-orange';
+                    break;
+            }
+            return icon;
+        },
+    },
+};
+
+// Components
+// Must be placed before the vue
+
+// vehicle-list
+Vue.component('vehicle-list', {
+    props: ['vehicles'],
+    template: '#vehicleList',
+    methods: {
+        selectVehicle(vehicle) {
+            console.log('vehicle-list received select-vehicle');
+            console.dir(vehicle);
+            this.$emit('select-vehicle', vehicle);
+        }
+    }
+});
+
+// vehicle-list-item
+Vue.component('vehicle-list-item', {
+    mixins: [vehicleMixin],
+    template: '#vehicleListItem',
+    methods: {
+        selectVehicle() {
+            console.log('vehicle-list-item sent select-vehicle');
+            this.$emit('select-vehicle', this.vehicle);
+        },
+    },
+});
+
+// vehicle-detail
+Vue.component('vehicle-detail', {
+    mixins: [vehicleMixin],
+    template: '#vehicleDetail',
+});
+
+// filters
+Vue.component('filters', {
+    props: ['vehicleCategories'],
+    computed: {
+        filters() {
+            let filters = ['none'];
+            filters = filters.concat(this.vehicleCategories);
+            return filters;
+        }
+    },
+    template: '#filters',
+});
+
+// TODO add computed icon (possibly using a mixin)
+// filter-element
+Vue.component('filter-element', {
+    props: ['filter'],
+    template: '#filterElement',
+});
+
+// Vue
+// Contains the components
+new Vue({
+    el: '#container',
+
+    data: {
+        vehicles: [],
+        // TODO vehicleCategories -> computed
+        vehicleCategories: [
+            "moto",
+            "voiture",
+            "van",
+            "velo"
+        ],
+        filters: [],
+        selectedVehicle: null,
+    },
+
+    computed: {
+        filteredVehicles() {
+            // if no filters, return all vehicles
+            if (this.filters === []) {
+                return this.vehicles;
+            }
+
+            //
+            return this.vehicles.filter(vehicle => this.filters.includes(vehicle.cat));
+        },
+    },
+
+    /**
+     * Initialize the data of the vue :
+     * - Load the vehicles list
+     * - select the first vehicle
+     */
+    mounted() {
+        // Load the content of the const array vehicles
+        this.vehicles = vehicules;
+        // select the first vehicle
+        this.selectedVehicle = this.vehicles[0];
+
+        // TODO remove test filter
+        this.filters = ['voiture'];
+    },
+
+    methods: {
+        selectVehicle(vehicle) {
+            this.selectedVehicle = vehicle;
+        }
+    }
+});
